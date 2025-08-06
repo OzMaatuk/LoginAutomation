33b286147bf319d9bcfa10b141445cc78e55a4cc
OzMaatuk <ozmaatuk@gmail.com>
2025-08-06 14:29:53 +0000

    fixes

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import configparser
-from login_automation.constants.constants import (
-    DEFAULT_HEADLESS,
-    DEFAULT_LOG_FORMAT,
-    DEFAULT_LOG_LEVEL,
-    DEFAULT_LOG_FILE,
-    DEFAULT_TIMEOUT,
-    SUPPORTED_PLATFORMS
-)
+import os
+from login_automation.constants.constants import Constants
 from login_automation.constants.linkedin import ConstantsLinkedIn
 
 
@@ -18,38 +12,38 @@ class AppConfig:
 
     def load_configurations(self):
         cfg = self.config
-        self.logging_format = cfg['logging'].get('logging_format', DEFAULT_LOG_FORMAT, raw=True)
-        self.log_level = cfg['logging'].get('log_level', DEFAULT_LOG_LEVEL)
-        self.log_file = cfg['logging'].get('log_file', DEFAULT_LOG_FILE)
-        self.timeout = cfg['driver'].getint('timeout', DEFAULT_TIMEOUT)
-        self.headless = cfg['driver'].getboolean('headless', DEFAULT_HEADLESS)
+        self.logging_format = cfg['logging'].get('logging_format', Constants.DEFAULT_LOGGING_FORMAT, raw=True)
+        self.log_level = cfg['logging'].get('log_level', Constants.DEFAULT_LOGGING_LEVEL)
+        self.log_file = cfg['logging'].get('log_file', Constants.DEFAULT_LOGGING_FILE)
+        self.timeout = cfg['driver'].getint('timeout', Constants.DEFAULT_TIMEOUT)
+        self.headless = cfg['driver'].getboolean('headless', Constants.DEFAULT_HEADLESS)
 
         # Load Credentials: (First from .env, else from config)
-        self.username = os.environ.get("USERNAME")
-        if not self.username:
-            self.username = config.get("user_info", "username", fallback="username")
-            if not self.username:
+        self.username: str = os.environ.get("USERNAME", "")
+        if self.username == "":
+            self.username = cfg.get("user_info", "username", fallback="")
+            if self.username == "":
                 raise ValueError("USERNAME is not set in environment variables or config.ini.")
 
-        self.password = os.environ.get("PASSWORD")
-        if not self.password:
-            self.password = config.get("user_info", "password", fallback="password")
-            if not self.password:
+        self.password = os.environ.get("PASSWORD", "")
+        if self.password == "":
+            self.password = cfg.get("user_info", "password", fallback="password")
+            if self.password == "":
                 raise ValueError("PASSWORD is not set in environment variables or config.ini.")
 
-        site_type = config.get("general", "site_type", fallback=None)
+        site_type = cfg.get("general", "site_type", fallback=None)
         self.login_url = None
         if not site_type:
             raise ValueError("site_type is not set in config.ini.")
-        supported_sites = SUPPORTED_PLATFORMS.keys()
+        supported_sites = Constants.SUPPORTED_PLATFORMS.keys()
         if site_type not in supported_sites:
-            logger.warning(f"site_type in config.ini is not supported. supported sites are {supported_sites}.")
-            logger.warning(f"try login to unsupported site.")
-            self.login_url = config.get("general", "login_url", fallback=None)
+            # logger.warning(f"site_type in config.ini is not supported. supported sites are {supported_sites}.")
+            # logger.warning(f"try login to unsupported site.")
+            self.login_url = cfg.get("general", "login_url", fallback=None)
             if not self.login_url:
-                raise raise ValueError("login_url is not configured in config.ini, it requiered due to not supported site_type.")
+                raise ValueError("login_url is not configured in config.ini, it requiered due to not supported site_type.")
         elif (site_type == "linkedin"):
             self.login_url = ConstantsLinkedIn.LOGIN_URL
             self.feed_url = ConstantsLinkedIn.FEED_URL
         else:
-            raise raise ValueError("login_url is not provided, you should set supported site_type, or provide login_url in config.ini")
+            raise ValueError("login_url is not provided, you should set supported site_type, or provide login_url in config.ini")
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from configparser import Error as ConfigParserError 
 import logging
+import sys
 from config import AppConfig
 
 from driver import PlaywrightDriver
 from logger import configure_application_logging
+from login_automation.login.factory import FactoryLogin
 
 logger = logging.getLogger(__name__)
 
@@ -11,31 +13,18 @@ logger = logging.getLogger(__name__)
 def main():
     try:
         logger.info("Starting main application.")
-        app_config = AppConfig('config.ini')
-        configure_application_logging(app_config.log_level, app_config.log_file, app_config.logging_format)
-
-        # Load user details
-        username = os.environ.get("USERNAME")
-        if not username:
-            username = config.get("user_info", "username", fallback="username")
-            if not username:
-                raise ValueError("USERNAME is not set in environment variables or config.ini.")
-    
-        password = os.environ.get("PASSWORD")
-        if not password:
-            password = config.get("user_info", "password", fallback="password")
-            if not password:
-                raise ValueError("PASSWORD is not set in environment variables or config.ini.")
+        cnf = AppConfig('config.ini')
+        configure_application_logging(cnf.log_level, cnf.log_file, cnf.logging_format)
 
         # Use context manager for driver
-        with PlaywrightDriver(app_config.headless, app_config.timeout) as driver:
+        with PlaywrightDriver(cnf.headless, cnf.timeout) as driver:
             if not driver.page:
                 raise Exception("Failed to initialize browser page")
                 
             app = FactoryLogin.create_login('linkedin', driver.page)
 
             # Execute
-            app.login(username, password)
+            app.login(cnf.username, cnf.password)
 
     except ConfigParserError as e:
         logger.error(f"Error reading configuration: {e}")
